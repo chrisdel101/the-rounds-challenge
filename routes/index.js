ad833c49e8f587b01781332589feb38ae929acb1
Chris Del <drumgod101@gmail.com>
2022-04-03 13:42:04 -0600

    working commit; fix bug

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,69 @@ var express = require('express')
 var router = express.Router()
 var wordController = require('../controllers/word.controller.js')
 const fetchedWords = []
+const deletedWords = []
+let successMsg = undefined
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  res.render('index', { fetchedWords: fetchedWords })
+  successMsg = undefined
+  // hack to solve continue undefined err``
+  if (!fetchedWords) fetchWords = []
+  res.render('index', {
+    fetchedWords,
+    successMsg: successMsg,
+    deletedWords,
+  })
 })
-router.get('/word', async function (req, res, next) {
-  const word = await wordController.httpsGetCall(
+router.post('/word', async function (req, res, next) {
+  const fetchedWord = await wordController.httpsGetCall(
     'https://words.boondoc.co/words'
   )
-  fetchedWords.push(word)
-  console.log('word', word)
-  console.log('fetched', fetchedWords)
-  res.render('index', { fetchedWords: fetchedWords })
+  fetchedWords.push(fetchedWord)
+  res.render('index', { fetchedWords, successMsg, deletedWords })
 })
-router.post('/', function (req, res, next) {
-  console.log('req', req.body)
-  res.render('index', { title: 'Express' })
+// post route
+router.post('/word', async function (req, res, next) {
+  // hack to solve continue undefined err``
+  if (!fetchedWords) fetchWords = []
+  const formData = req.body['word-post']
+  if (!formData) {
+    successMsg = 'Cannot add blank word'
+    res.render('index', {
+      fetchedWords,
+      successMsg,
+      deletedWords,
+    })
+  }
+
+  const pushedWord = await wordController.httpPostCall(
+    'https://words.boondoc.co/words',
+    formData
+  )
+  if (!pushedWord) {
+    successMsg = 'An error occured'
+    res.render('index', {
+      fetchedWords,
+      deletedWords,
+      successMsg: successMsg,
+    })
+  }
+
+  if (/2\d\d$/.test(res.statusCode.toString())) {
+    successMsg = 'success adding word'
+    res.render('index', {
+      fetchedWords,
+      successMsg,
+      deletedWords,
+    })
+  }
+})
+// delete route
+router.get('/delete-word', async function (req, res, next) {
+  const deleteWord = await wordController.httpDeleteCall(
+    'https://words.boondoc.co/words'
+  )  if (!deletedWords) deletedWords = []
+  deletedWords.push(deleteWord)
+  res.render('index', { fetchedWords, successMsg, deletedWords })
 })
 
 module.exports = router
